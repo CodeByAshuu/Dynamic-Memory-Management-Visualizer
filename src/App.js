adfe92459a185fd2a578548d439883a2646a5790
Sagar <141819298+CodeByAshuu@users.noreply.github.com>
2025-03-25 23:56:46 +0530

    Create App.js
    default App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Simulation from "./simulation"; // Importing main logic file
+
+function App() {
+  return (
+    <div>
+      <Simulation />
+    </div>
+  );
+}
+
+export default App;
